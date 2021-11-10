5e1bae6b794473ff1e0da6708a4d41c56a61ea12
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-11-10 15:24:47 +0100

    survey class start

--- SurveyLib/Survey.cs
+++ SurveyLib/Survey.cs
@@ -1,7 +1,62 @@
+using System.Collections.Generic;
+
 namespace SurveyLib
 {
     public class Survey
     {
-        
+        string title;
+
+        string id;
+
+        List<Question> questions;
+
+        IDataManager datamanager;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public IList<Question> Questions
+        {
+            get
+            {
+                return questions.AsReadOnly();
+            }
+        }
+
+        public Survey(string title, string id, IDataManager datamanager)
+        {
+            this.title = title;
+            this.id = id;
+            this.datamanager = datamanager;
+            questions = new List<Question>();
+        }
+
+        public void AddQuestion(Question question)
+        {
+            questions.Add(question);
+        }
     }
 }
