c93ac43581b6cf5b6c172be345e4e02c3dc784d7
OdiousCode <gorgande@gmail.com>
2021-11-11 09:34:59 +0100

    tst

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,13 @@ namespace ConsoleUI
             User testAdminUser = new User("199001014444", UserRoles.Admin);
             User testUserUser = new User("198002025555", UserRoles.User);
             UserList userList = new UserList();
+            userList.AddNewUser(testAdminUser);
+            userList.AddNewUser(testUserUser);
+            foreach (var item in userList.ListUsers())
+            {
+                System.Console.WriteLine("Personnummer: " + item.Ssn + " usertype: " + item.GetType());
+            }
+            userList.ListUsers();
         }
     }
 }
