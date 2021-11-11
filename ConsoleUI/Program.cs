ab368e3ee8a7ccf3ab3f75d7a4d9bb6ab8278518
MR-ALISSON <mah73mah@gmail.com>
2021-11-11 10:35:56 +0100

    Added Menu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,12 +7,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            User testAdminUser = new User("1990-01-01-4444", UserRoles.Admin);
-            User testUserUser = new User("1980-02-02-5555", UserRoles.User);
-            UserList userList = new UserList();
-            userList.AddNewUser(testAdminUser);
-            userList.AddNewUser(testUserUser);
-            userList.ListUsers();
+            
+            Login login = new();
+            login.LoginRun();
         }
     }
 }
