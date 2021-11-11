da108910c62b8c99bfa48b74bb7edb4ad9ab036a
OdiousCode <gorgande@gmail.com>
2021-11-11 09:13:01 +0100

    List of users

---
+++ SurveyLib/UserList.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+namespace SurveyLib
+{
+
+
+    public class UserList
+    {
+        private List<User> userList;
+        public int userCount;
+        public UserList()
+        {
+            userList = new();
+        }
+        public void AddNewUser(User user)
+        {
+            userList.Add(user);
+            userCount = userList.Count;
+        }
+        public List<User> ListUsers()
+        {
+            List<User> userList = new();
+            foreach (User u in userList)
+            {
+                userList.Add(u);
+            }
+            return userList;
+        }
+    }
+}
