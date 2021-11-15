1083b1ac9a66ec08ec9b72256d5afd160738ad14
Ola Larsson <soaring.jr@hotmail.com>
2021-11-15 14:23:57 +0100

    savesurvey done

--- SurveyLib/DataManager.cs
+++ SurveyLib/DataManager.cs
@@ -1,38 +1,87 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
 namespace SurveyLib
 {
     public class DataManager
     {
-        /* public void SaveSurvey(Survey survey)
-        {
+        string sqlConnection = "Server = 40.85.84.155; Database=OOP_BLÅ;User=Student27;Password=big-bada-boom!";
 
+        private int SaveQuestion(int primaryKey, int type, string title)
+        {
+            using (SqlConnection connection = new(sqlConnection))
+            {
+                connection.Execute("INSERT INTO Question(Survey_ID, Type, Title) VALUES (@Survey_ID, @Type, @Title);", new { Survey_ID = primaryKey, Type = type, Title = title });
+                List<int> qList = connection.Query<int>("SELECT IDENT_CURRENT('Question');").ToList();
+                return qList[0];
+            }
         }
 
-        public Survey LoadSurvey(int surveyCode)
+        public void SaveSurvey(Survey survey)
         {
+            List<Question> questionList = survey.GetQuestions();
+            int primaryKey;
+            int questionPrimaryKey;
+            //int multiChoiceID;
+
+            using (SqlConnection connection = new(sqlConnection))
+            {
+                connection.Execute("INSERT INTO Survey (Title) VALUES (@Title);", new { Title = survey.Title });
+                List<int> pList = connection.Query<int>("SELECT IDENT_CURRENT('Survey');").ToList();
+                primaryKey = pList[0];
+            }
+
+
+            foreach (Question question in questionList)
+            {
+                if (question is MultipleChoiseQuestion)
+                {
+                    MultipleChoiseQuestion q = question as MultipleChoiseQuestion;
+                    int type = 1;
+
+                    questionPrimaryKey = SaveQuestion(primaryKey, type, q.Title);
+
+                    for (int i = 0; i < q.GetOptions().Count; i++)
+                    {
+                        string alternative = q.GetOptions()[i];
+                        //bool answer = q.GetAnswer()[i];
+
+                        using (SqlConnection connection = new(sqlConnection))
+                        {
+                            connection.Execute("INSERT INTO Multiple_Choice_Question(Question_ID, Alternative) VALUES (@Question_ID, @Alternative)", new { Question_ID = questionPrimaryKey, Alternative = alternative });
+                        }
+                    }
+
+                }
+
+                else if (question is FreetextQuestion)
+                {
+                    FreetextQuestion f = question as FreetextQuestion;
+                    int type = 2;
+
+                    questionPrimaryKey = SaveQuestion(primaryKey, type, f.Title);
+                }
+
+                else if (question is YesOrNoQuestion)
+                {
+                    YesOrNoQuestion y = question as YesOrNoQuestion;
+                    int type = 3;
+
+                    questionPrimaryKey = SaveQuestion(primaryKey, type, y.Title);
+                }
+
+                else if (question is _1_to_10)
+                {
+                    _1_to_10 scale = question as _1_to_10;
+                    int type = 4;
+
+                    questionPrimaryKey = SaveQuestion(primaryKey, type, scale.Title);
+                }
+            }
 
         }
-
-        public void SaveUser(User user)
-        {
-
-        }
-
-        public User LoadUser(string PersonNummer)
-        {
-
-        }
-
-        public void SaveUser_Survey(User_Survey user_Survey)
-        {
-
-        }
-
-        public User_Survey LoadUser_Survey(string Code)
-        {
-
-        }
-        */
-
 
     }
 }
