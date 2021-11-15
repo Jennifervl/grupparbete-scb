140a85428ddc58b1b309fc5f825ff72b57baeda1
MR-ALISSON <mah73mah@gmail.com>
2021-11-15 11:43:12 +0100

    added nice menu

--- ConsoleUI/Menu.cs
+++ ConsoleUI/Menu.cs
@@ -67,6 +67,15 @@ namespace ConsoleUI
             while (adminRun == true)
             {
                 Console.Clear();
+                 System.Console.WriteLine(@"
+    ███████ ███████  ██████ ██████  ███████ ████████      █████  ██████  ███    ███ ██ ███    ██     ███    ███ ███████ ███    ██ ██    ██ 
+    ██      ██      ██      ██   ██ ██         ██        ██   ██ ██   ██ ████  ████ ██ ████   ██     ████  ████ ██      ████   ██ ██    ██ 
+    ███████ █████   ██      ██████  █████      ██        ███████ ██   ██ ██ ████ ██ ██ ██ ██  ██     ██ ████ ██ █████   ██ ██  ██ ██    ██ 
+         ██ ██      ██      ██   ██ ██         ██        ██   ██ ██   ██ ██  ██  ██ ██ ██  ██ ██     ██  ██  ██ ██      ██  ██ ██ ██    ██ 
+    ███████ ███████  ██████ ██   ██ ███████    ██        ██   ██ ██████  ██      ██ ██ ██   ████     ██      ██ ███████ ██   ████  ██████  
+                                                                                                                                           
+                                                                                                                                           
+");
                 System.Console.WriteLine("Welcome to the super secret admin menu");
                 System.Console.WriteLine("[A] Create a survey");
                 System.Console.WriteLine("[L] List all surveys");
