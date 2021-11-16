9c92dc84e238fed8c925b93a6b082d4f4296a0eb
Ola Larsson <soaring.jr@hotmail.com>
2021-11-16 10:31:24 +0100

    Fixed LoadSurvey

--- SurveyLib/DataManager.cs
+++ SurveyLib/DataManager.cs
@@ -29,8 +29,7 @@ namespace SurveyLib
             using (SqlConnection connection = new(sqlConnection))
             {
                 connection.Execute("INSERT INTO Survey (Title) VALUES (@Title);", new { Title = survey.Title });
-                List<int> pList = connection.Query<int>("SELECT IDENT_CURRENT('Survey');").ToList();
-                primaryKey = pList[0];
+                primaryKey = connection.QueryFirstOrDefault<int>("SELECT IDENT_CURRENT('Survey');");
             }
 
 
@@ -78,10 +77,92 @@ namespace SurveyLib
                     int type = 4;
 
                     questionPrimaryKey = SaveQuestion(primaryKey, type, scale.Title);
+
+                    using (SqlConnection connection = new(sqlConnection))
+                    {
+                        connection.Execute("INSERT INTO Scale_Question(Question_ID, Value_1, VALUE_10) VALUES (@Question_ID, @Value_1, @Value_10);", new { Question_ID = questionPrimaryKey, Value_1 = scale.Value1, Value_10 = scale.Value10 });
+                    }
                 }
             }
 
         }
 
+        public Survey UserLoadSurvey(int pKey) //string userCode ska vara här
+        {
+            int primaryKey = pKey;
+            string title;
+            List<int> questionKeys = new();
+            string questionText;
+
+            using (SqlConnection connection = new(sqlConnection))
+            {
+                //primaryKey = connection.QueryFirstOrDefault<int>("SELECT Survey_ID FROM User_Survey WHERE User_Specific_Code = @User_Specific_Code;", new { User_Specific_Code = UserCode });
+
+                title = connection.QueryFirstOrDefault<string>("SELECT Title FROM Survey WHERE ID = @ID", new { ID = primaryKey });
+            }
+
+            Survey loadedSurvey = new(title);
+
+            using (SqlConnection connection = new(sqlConnection))
+            {
+                questionKeys = connection.Query<int>("SELECT ID FROM Question WHERE Survey_ID = @Survey_ID;", new { Survey_ID = primaryKey }).ToList();
+            }
+
+            foreach (int key in questionKeys)
+            {
+                int questionType;
+
+                using (SqlConnection connection = new(sqlConnection))
+                {
+                    questionType = connection.QueryFirstOrDefault<int>("SELECT Type FROM Question WHERE ID = @ID;", new { ID = key });
+                    //questionType = qKeyList[0];
+                    questionText = connection.QueryFirstOrDefault<string>("SELECT QuestionText FROM QUESTION WHERE ID = @ID;", new { ID = key });
+                }
+
+                if (questionType == 1) //Multiple Choice
+                {
+                    List<string> alternatives = new();
+
+                    using (SqlConnection connection = new(sqlConnection))
+                    {
+                        alternatives = connection.Query<string>("SELECT Alternative FROM Multiple_Choice_Question WHERE Question_ID = @Question_ID", new { Question_ID = key }).ToList();
+                    }
+
+                    MultipleChoiseQuestion multipleChoiseQuestion = new(questionText, alternatives);
+
+                    loadedSurvey.AddQuestion(multipleChoiseQuestion);
+                }
+                else if (questionType == 2) //FreeTextQuestion
+                {
+                    FreetextQuestion freetextQuestion = new(questionText);
+
+                    loadedSurvey.AddQuestion(freetextQuestion);
+                }
+
+                else if (questionType == 3) //YesOrNoQuestion
+                {
+                    YesOrNoQuestion yesOrNoQuestion = new(title);
+
+                    loadedSurvey.AddQuestion(yesOrNoQuestion);
+                }
+
+                else if (questionType == 4)//ScaleQuestion
+                {
+                    string value1;
+                    string value10;
+
+                    using (SqlConnection connection = new(sqlConnection))
+                    {
+                        value1 = connection.QueryFirstOrDefault<string>("SELECT Value_1 FROM Scale_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key });
+                        value10 = connection.QueryFirstOrDefault<string>("SELECT Value_10 FROM Scale_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key });
+                    }
+
+                    _1_to_10 scaleQuestion = new(questionText, value1, value10);
+
+                    loadedSurvey.AddQuestion(scaleQuestion);
+                }
+            }
+            return loadedSurvey;
+        }
     }
 }
