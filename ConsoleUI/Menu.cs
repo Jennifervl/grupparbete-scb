39287cbee92088af2b4fedf914617561df4c5db6
Jennifer Johansson Pawlus <jennie_jp@hotmail.com>
2021-11-16 10:27:53 +0100

    ListUsers in menu

--- ConsoleUI/Menu.cs
+++ ConsoleUI/Menu.cs
@@ -99,6 +99,7 @@ namespace ConsoleUI
                 System.Console.WriteLine("[T] Test a survey");
                 Console.WriteLine("[D] Distribute survey");
                 Console.WriteLine("[S] Show all distributions");
+                Console.WriteLine("[U] List all users");
                 System.Console.WriteLine("[X] Return to main menu");
                 string adminChoice = Console.ReadKey(true).Key.ToString().ToLower();
                 switch (adminChoice)
@@ -167,6 +168,17 @@ namespace ConsoleUI
                             {
                                 Console.WriteLine(entry.Key + " " + entry.Value);
 
+                            }
+                            Console.ReadLine();
+                            break;
+                        }
+
+                    case "u":
+                        {
+                            Dictionary<string, UserRoles> users = userList.ListUsers();
+                            foreach (KeyValuePair<string, UserRoles> u in users)
+                            {
+                                Console.WriteLine(u.Key + " " + u.Value.ToString());
                             }
                             Console.ReadLine();
                             break;
