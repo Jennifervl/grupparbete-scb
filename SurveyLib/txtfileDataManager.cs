b10bdb728af675f747b923c5269ac7868bd1d418
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-11-16 11:47:04 +0100

    SaveSurveyDataToText

--- SurveyLib/txtfileDataManager.cs
+++ SurveyLib/txtfileDataManager.cs
@@ -8,10 +8,66 @@ namespace SurveyLib
     {
         public static void SaveResult(Survey survey)
         {
-            if (File.Exists("surveyData\"" + survey.Title))
+            if (!(File.Exists($"..\\SurveyLib\\surveyData\\" + $"{survey.Title}")))
             {
-
+                File.WriteAllText($"..\\SurveyLib\\surveyData\\" + $"{survey.Title}", BuildSurveyTxtFileString(survey));
             }
+
+            if (File.Exists($"..\\SurveyLib\\surveyData\\" + $"{survey.Title}"))
+            {
+                string data = File.ReadAllText($"..\\SurveyLib\\surveyData\\" + $"{survey.Title}");
+                int i = 1;
+                foreach (Question q in survey.GetQuestions())
+                {
+                    if (q is _1_to_10 oneToTen)
+                    {
+                        data = data.Insert(data.IndexOf("[", GetNthIndex(data, '[', i)) + 1, oneToTen.Answer.ToString());
+                    }
+                    else if (q is FreetextQuestion ftq)
+                    {
+                        data = data.Insert(data.IndexOf("[", GetNthIndex(data, '[', i)) + 1, ftq.GetAnswer() + "£");
+                    }
+                    else if (q is MultipleChoiseQuestion mcq)
+                    {
+                        foreach (bool answer in mcq.GetAnswer())
+                        {
+                            data = data.Insert(data.IndexOf("[", GetNthIndex(data, '[', i)) + 1, answer + ",");
+                        }
+                    }
+                    else if (q is YesOrNoQuestion yesOrNo)
+                    {
+                        data = data.Insert(data.IndexOf("[", GetNthIndex(data, '[', i)) + 1, yesOrNo.GetAnswer().ToString() + ",");
+                    }
+                    i++;
+                }
+                File.WriteAllText($"..\\SurveyLib\\surveyData\\" + $"{survey.Title}", data);
+            }
+        }
+
+        public static string BuildSurveyTxtFileString(Survey survey)
+        {
+            string txtfile = $":{survey.Title}:\n";
+            foreach (Question q in survey.GetQuestions())
+            {
+                txtfile += "[]\n";
+            }
+            return txtfile;
+        }
+        private static int GetNthIndex(string s, char t, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == t)
+                {
+                    count++;
+                    if (count == n)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
