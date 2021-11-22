b80366ea72b4f8e084e6e7673f90e32b7146a11f
Ola Larsson <soaring.jr@hotmail.com>
2021-11-22 10:03:21 +0100

    added loadsurveyanswers

--- SurveyLib/DataManagers/LoadDataManager.cs
+++ SurveyLib/DataManagers/LoadDataManager.cs
@@ -199,12 +199,84 @@ namespace SurveyLib
 
                 Survey loadedSurvey = new(surveyTitle);
 
-                List<int> questionKeys = connection.Query<int>("SELECT ID FROM Question WHERE Survey_ID = @Survey_ID", new { Survey_ID = surveyKey }).ToList();
+                List<int> questionKeys = connection.Query<int>("SELECT ID FROM Question WHERE Survey_ID = @Survey_ID;", new { Survey_ID = surveyKey }).ToList();
 
                 foreach (int key in questionKeys)
                 {
-                    int type = connection.QueryFirstOrDefault<int>("SELECT ID FROM Question WHERE Survey_ID = @Survey_ID", new { Survey_ID = surveyKey });
+                    int type = connection.QueryFirstOrDefault<int>("SELECT Type FROM Question WHERE ID = @ID;", new { ID = key });
+
+                    if (type == 1)//Multiple Choice Question
+                    {
+                        string questionText = connection.QueryFirstOrDefault<string>("SELECT QuestionText FROM Question WHERE ID = @ID;", new { ID = key });
+                        List<string> alternatives = connection.Query<string>("SELECT Alternative FROM Multiple_Choice_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key }).ToList();
+                        List<int> alternativeKeys = connection.Query<int>("SELECT ID FROM Multiple_Choice_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key }).ToList();
+                        List<List<bool>> mcqAnswerList = new();
+
+                        foreach (int altKey in alternativeKeys)
+                        {
+                            List<bool> mcqAnswer = connection.Query<bool>("SELECT Answer FROM Mutiple_Choice_Answer WHERE Multiple_Choice_Question_ID = @Multiple_Choice_Question_ID;", new { Multiple_Choice_Question_ID = altKey }).ToList();
+                            mcqAnswerList.Add(mcqAnswer);
+                        }
+
+                        MultipleChoiseQuestion mcq = new(questionText, alternatives);
+                        foreach (List<bool> answer in mcqAnswerList)
+                        {
+                            mcq.SetAnswerFromDB(answer);
+                        }
+
+                        loadedSurvey.AddQuestion(mcq);
+                    }
+
+                    else if (type == 2)//FreeText
+                    {
+                        string questionText = connection.QueryFirstOrDefault<string>("SELECT QuestionText FROM Question WHERE ID = @ID;", new { ID = key });
+                        List<string> freeTextAnswers = connection.Query<string>("SELECT Answer FROM Free_Text_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key }).ToList();
+
+                        FreetextQuestion ftq = new(questionText);
+
+                        foreach (string answer in freeTextAnswers)
+                        {
+                            ftq.SetAnswerFromDB(answer);
+                        }
+
+                        loadedSurvey.AddQuestion(ftq);
+                    }
+
+                    else if (type == 3)//YesOrNo
+                    {
+                        string questionText = connection.QueryFirstOrDefault<string>("SELECT QuestionText FROM Question WHERE ID = @ID;", new { ID = key });
+                        List<bool> yesOrNoAnswers = connection.Query<bool>("SELECT Answer FROM True_False_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key }).ToList();
+
+                        YesOrNoQuestion yonq = new(questionText);
+
+                        foreach (bool answer in yesOrNoAnswers)
+                        {
+                            yonq.SetAnswerFromDB(answer);
+                        }
+
+                        loadedSurvey.AddQuestion(yonq);
+                    }
+
+                    else if (type == 4)//Scale question
+                    {
+                        string questionText = connection.QueryFirstOrDefault<string>("SELECT QuestionText FROM Question WHERE ID = @ID;", new { ID = key });
+                        string value10 = connection.QueryFirstOrDefault<string>("SELECT Value_10 FROM Scale_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key });
+                        string value1 = connection.QueryFirstOrDefault<string>("SELECT Value_1 FROM Scale_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key });
+                        int scaleKey = connection.QueryFirstOrDefault<int>("SELECT ID FROM Scale_Question WHERE Question_ID = @Question_ID;", new { Question_ID = key });
+                        List<int> scaleAnswers = connection.Query<int>("SELECT Answer FROM Scale_Question_Answer WHERE Scale_Question_ID = @Scale_Question_ID;", new { Scale_Question_ID = scaleKey }).ToList();
+
+
+                        _1_to_10 scaleQuestion = new(questionText, value1, value10);
+
+                        foreach (int answer in scaleAnswers)
+                        {
+                            scaleQuestion.SetAnswerFromDB(answer);
+                        }
+
+                        loadedSurvey.AddQuestion(scaleQuestion);
+                    }
                 }
+
                 return loadedSurvey;
             }
         }
